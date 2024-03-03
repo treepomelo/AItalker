6e0f3407a726bef18defefcd6b961972085c7fa2
oujf <oujf@caigouku.com>
2024-03-03 14:06:48 +0800

    更新功能 修复BUG 优化

--- .java/src/main/java/com/cn/app/plugIns/DallDrawingPlugIn.java
+++ .java/src/main/java/com/cn/app/plugIns/DallDrawingPlugIn.java
@@ -54,10 +54,10 @@ public class DallDrawingPlugIn {
      * @param dto the prompt
      * @return the string
      */
-    public JSONObject GENERATE_IMAGE(final String dto) {
+    public JSONObject GENERATE_IMAGE(final String dto, String proxyAddress, String authToken) {
         final String block = WebClient.builder()
-                .baseUrl("https://www.lxlchat.top/v1/images/generations")
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer your-api-key")
+                .baseUrl(proxyAddress + "/images/generations")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken)
                 .codecs(item -> item.defaultCodecs().maxInMemorySize(20 * 1024 * 1024))
                 .build()
                 .post()
